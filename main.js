aaf2b4eae64f20569826dafdf7d4f2ef80ba33ff
mikky-cecil <mikkycecil@gmail.com>
2017-09-12 03:41:33 -0700

    there are trees now. and contact info. it is looking nice.

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+var timeOuts = [];
+
 var twinkle = function(star){
-	setTimeout(function(){
+	timeOuts.push(setTimeout(function(){
 		star.toggle(500);
 		star.toggle(500);
 		twinkle(star);
-	}, Math.floor(Math.random() * 10000));
+	}, Math.floor(Math.random() * 15000)));
 }
 
 var buildStars = function(container){
@@ -14,7 +16,8 @@ var buildStars = function(container){
  			top -= (top*.5);
  		}
 
- 		return Math.floor(top);
+ 		// return Math.floor(top);
+ 		return top;
  	}
 
  	var getColor = function(){
@@ -27,26 +30,18 @@ var buildStars = function(container){
  			"rgba(255, 240, 240, 1)"][Math.floor(Math.random() * 6)]; /* yellow */
  	}
 
- 	/* make sure the stars are underneath the content */
- 	container.find(".my-content").css("z-index", 1);
-
- 	for (var i = 0; i < 400; i++){
- 		if (i < 200){
-	 		var size = Math.floor(Math.random() * 3) * 2;
-	 	}else{
-	 		var size = 2;
-	 	}
- 		var color = getColor();
- 		var top = getTop();
- 		var left = Math.floor(Math.random() * 100);
- 		var g = "(" + color + ", rgba(0,0,0,0))";
- 		var starColors = {
+ 	var getStarColors = function(g){
+ 		return {
  			"background": "-webkit-radial-gradient" + g, /* Safari 5.1- 6.0 */
   			"background": "-o-radial-gradient" + g, /* Opera 11.6-12.0 */
   			"background": "-moz-radial-gradient" + g, /* Firefox 3.6-15 */
   			"background": "radial-gradient" + g
- 		}
- 		var star = $("<div class=\"star\"></div>").css($.extend({
+ 		};
+ 	}
+
+ 	var getStarCSS = function(size, top, left, color){
+ 		var g = "(" + color + " 25%, rgba(0,0,0,0)) 100%";
+ 		return $.extend({
  			"display": "block",
  			"background": color,
  			"position": "absolute",
@@ -58,15 +53,84 @@ var buildStars = function(container){
  			"-moz-border-radius": (size/2) + "px",
  			"-webkit-border-radius": (size/2) + "px",
  			"border-radius": (size/2) + "px"
- 		}, starColors));
- 		container.append(star);
+ 		}, getStarColors(g));
+ 	}
 
- 		if (size >= 2){
+ 	/* make sure the stars are underneath the content */
+ 	container.find(".my-content").css("z-index", 1);
+ 	var sky = $("<div id=\"star-container\"></div>");
+ 	container.append(sky);
+
+ 	/* generate stars */
+ 	for (var i = 0; i < 400; i++){
+ 		if (i < 200){
+	 		var size = Math.floor(Math.random() * 3) * 2;
+	 	}else{
+	 		var size = 2;
+	 	}
+ 		var color = getColor();
+ 		var top = getTop();
+ 		var left = Math.random() * 100;
+ 		var star = $("<div class=\"star\"></div>").css(getStarCSS(size, top, left, color));
+ 		sky.append(star);
+
+ 		/* make some of them twinkle */
+ 		if (i <= 100){
 	 		with ({x:star}){
 		 		twinkle(x);
 		 	}
 		}
  	}
+
+	var sky = $("<div id=\"comet-container\"></div>");
+ 	container.append(sky);
+
+ 	var makeComet = function(){
+	 	/* Comets */
+		// TODO: this needs work
+		var color = "#FFFFFF";
+		var left = Math.floor(Math.random() * 75) + 25; /* comets will go right to left */
+		var comet = $("<div class=\"comet\"></div>")
+			.css($.extend(getStarCSS(6, 0, left, color), {
+				"box-shadow": "4px -4px 5px rgba(200, 225, 255, 1)"
+			}));
+		sky.append(comet);
+		comet.animate({
+			"top": "70%",
+			"left": (left - 50) + "%"
+		}, {duration: 1500, queue: false, complete: function(){
+			comet.remove();
+		}});
+		// comet.animate({
+		// 	"opacity": "0"
+		// }, {duration: 1500, queue: false});
+		comet.toggle(1500);
+
+		/* wait for the next comet */
+		timeOuts.push(setTimeout(function(){
+			makeComet();
+		}, Math.floor(Math.random() * 15000)));
+	};
+
+	makeComet();
+};
+
+var stopTwinkling = function(){
+	console.log("gunna stop");
+	$(timeOuts).each(function(timeOut){
+		clearTimeout(timeOut);
+		timeOuts = [];
+	});
+};
+
+var restartTwinkling = function () {
+	$('.star').each(function(i, star){
+		if (i > 100){
+			return false;
+		}
+		// console.log($(star));
+		twinkle($(star));
+	});
 };
 
 $(document).ready(function(){
@@ -81,9 +145,26 @@ $(document).ready(function(){
 		}, 900, 'swing');
 	});
 
-	buildStars($('#about'));
+	/* inactive tab, stop twinkling */
+	$(window).blur(function(){
+		stopTwinkling();
+	});
 
-	// window.setTimeout(function(){
-	// 	$(".star").css({"width": "50px", "height": "50px"});
-	// }, 5000);
+	/* active tab, start twinkling again*/
+	$(window).focus(function(){
+		restartTwinkling();
+	});
+
+	$('#twinkleToggle').data({'twinkles': 'on'});
+	$('#twinkleToggle').click(function(){
+		if ($(this).data('twinkles') == 'on'){
+			stopTwinkling();
+			$(this).html("<i class=\"em em-sparkles\"></i>").data({'twinkles': 'off'});
+		}else{
+			restartTwinkling();
+			$(this).html("Stop Twinkling").data({'twinkles': 'on'});
+		}
+	});
+
+	buildStars($('#about'));
 });
