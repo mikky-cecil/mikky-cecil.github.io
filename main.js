d4a23533f6a5aa51ee70f3d4a04863381953d663
mikky-cecil <mikkycecil@gmail.com>
2017-09-11 19:07:38 -0700

    Added sunset and stars to about section. flower background is projects now

--- main.js
+++ main.js
@@ -1,3 +1,62 @@
+var buildStars = function(container){
+ //   width: 140px;
+ //   height: 140px;
+ //   background: blue; 
+ //   -moz-border-radius: 70px; 
+ //   -webkit-border-radius: 70px; 
+ //   border-radius: 70px;
+
+ 	var getTop = function(){
+ 		var top = Math.random() * 70;
+ 		if ((Math.random() * 100) >= 25){
+ 			console.log("sub");
+ 			top -= (top*.5);
+ 		}
+
+ 		return Math.floor(top);
+ 	}
+
+ 	var getColor = function(){
+ 		return [
+ 			"rgba(255, 255, 255, 1)", /* white */
+ 			"rgba(255, 255, 255, 1)", /* white */
+ 			"rgba(255, 255, 255, 1)", /* white */
+ 			"rgba(200, 225, 255, 1)", /* blue */
+ 			"rgba(255, 225, 200, 1)", /* red */
+ 			"rgba(255, 240, 240, 1)"][Math.floor(Math.random() * 6)]; /* yellow */
+ 	}
+
+ 	container.find(".my-content").css("z-index", 1);
+
+ 	for (var i = 0; i < 400; i++){
+ 		if (i < 200){
+	 		var size = Math.floor(Math.random() * 3) * 2;
+	 	}else{
+	 		var size = 2;
+	 	}
+ 		var color = getColor();
+ 		var top = getTop();
+ 		var left = Math.floor(Math.random() * 100);
+ 		var g = "(" + color + ", rgba(0,0,0,0))";
+ 		container.append($("<div class=\"star\"></div>").css({
+ 			"position": "absolute",
+ 			"z-index": "0",
+ 			"top": top + "%",
+ 			"left": left + "%",
+ 			"width": size + "px",
+ 			"height": size + "px",
+ 			"background": color,
+ 			"-moz-border-radius": (size/2) + "px",
+ 			"-webkit-border-radius": (size/2) + "px",
+ 			"border-radius": (size/2) + "px",
+ 			"background": "-webkit-radial-gradient" + g, /* Safari 5.1- 6.0 */
+  			"background": "-o-radial-gradient" + g, /* Opera 11.6-12.0 */
+  			"background": "-moz-radial-gradient" + g, /* Firefox 3.6-15 */
+  			"background": "radial-gradient" + g
+ 		}));
+ 	}
+};
+
 $(document).ready(function(){
 	$('a[href^="#"]').on('click', function(e){
 		e.preventDefault();
@@ -7,8 +66,12 @@ $(document).ready(function(){
 
 		$('html, body').stop().animate({
 			'scrollTop': $target.offset().top
-		}, 900, 'swing', function(){
-			window.location.hash = target;
-		});
+		}, 900, 'swing');
 	});
+
+	buildStars($('#about'));
+
+	// window.setTimeout(function(){
+	// 	$(".star").css({"width": "50px", "height": "50px"});
+	// }, 5000);
 });
